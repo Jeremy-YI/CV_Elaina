902a06f3208a4dbdf59649f52c2d51bd29cc7126
Jeremy Yi <yiyanhong10233@gmail.com>
2022-12-04 13:05:46 +1100

    Initial commit

---
+++ index.js
@@ -0,0 +1,333 @@
+const images = [
+  {
+    id: 1,
+    category: "women",
+    image: "./assets/Image/Women/women1.jpeg",
+    time: "01 / 01 / 2021",
+  },
+  {
+    id: 2,
+    category: "women",
+    image: "./assets/Image/Women/women2.jpeg",
+    time: "01 / 01 / 2021",
+  },
+  {
+    id: 3,
+    category: "women",
+    image: "./assets/Image/Women/women3.jpeg",
+    time: "01 / 01 / 2021",
+  },
+  {
+    id: 4,
+    category: "women",
+    image: "./assets/Image/Women/women4.jpeg",
+    time: "01 / 01 / 2021",
+  },
+  {
+    id: 5,
+    category: "women",
+    image: "./assets/Image/Women/women5.jpeg",
+    time: "01 / 01 / 2021",
+  },
+  {
+    id: 6,
+    category: "women",
+    image: "./assets/Image/Women/women6.jpeg",
+    time: "01 / 01 / 2021",
+  },
+  {
+    id: 7,
+    category: "women",
+    image: "./assets/Image/Women/women7.jpeg",
+    time: "01 / 01 / 2021",
+  },
+  {
+    id: 8,
+    category: "women",
+    image: "./assets/Image/Women/women8.jpeg",
+    time: "01 / 01 / 2021",
+  },
+  {
+    id: 9,
+    category: "women",
+    image: "./assets/Image/Women/women9.jpeg",
+    time: "01 / 01 / 2021",
+  },
+  {
+    id: 10,
+    category: "women",
+    image: "./assets/Image/Women/women10.jpeg",
+    time: "01 / 01 / 2021",
+  },
+  {
+    id: 11,
+    category: "men",
+    image: "./assets/Image/men/men1.jpeg",
+    time: "01 / 01 / 2021",
+  },
+  {
+    id: 12,
+    category: "men",
+    image: "./assets/Image/men/men2.jpeg",
+    time: "01 / 01 / 2021",
+  },
+  {
+    id: 13,
+    category: "men",
+    image: "./assets/Image/men/men3.jpeg",
+    time: "01 / 01 / 2021",
+  },
+  {
+    id: 14,
+    category: "men",
+    image: "./assets/Image/men/men4.jpeg",
+    time: "01 / 01 / 2021",
+  },
+  {
+    id: 15,
+    category: "men",
+    image: "./assets/Image/men/men5.jpeg",
+    time: "01 / 01 / 2021",
+  },
+  {
+    id: 16,
+    category: "men",
+    image: "./assets/Image/men/men6.jpeg",
+    time: "01 / 01 / 2021",
+  },
+  {
+    id: 17,
+    category: "men",
+    image: "./assets/Image/men/men7.jpeg",
+    time: "01 / 01 / 2021",
+  },
+  {
+    id: 18,
+    category: "men",
+    image: "./assets/Image/men/men8.jpeg",
+    time: "01 / 01 / 2021",
+  },
+  {
+    id: 19,
+    category: "bespoke",
+    image: "./assets/Image/Bespoke/bespoke1.jpeg",
+    time: "01 / 01 / 2021",
+  },
+  {
+    id: 20,
+    category: "bespoke",
+    image: "./assets/Image/Bespoke/bespoke2.jpeg",
+    time: "01 / 01 / 2021",
+  },
+  {
+    id: 21,
+    category: "bespoke",
+    image: "./assets/Image/Bespoke/bespoke3.jpeg",
+    time: "01 / 01 / 2021",
+  },
+  {
+    id: 22,
+    category: "bespoke",
+    image: "./assets/Image/Bespoke/bespoke4.jpeg",
+    time: "01 / 01 / 2021",
+  },
+  {
+    id: 23,
+    category: "bespoke",
+    image: "./assets/Image/Bespoke/bespoke5.jpeg",
+    time: "01 / 01 / 2021",
+  },
+  {
+    id: 24,
+    category: "bespoke",
+    image: "./assets/Image/Bespoke/bespoke6.jpeg",
+    time: "01 / 01 / 2021",
+  },
+  {
+    id: 25,
+    category: "kitting",
+    image: "./assets/Image/MachineKnitting/kitting1.jpeg",
+    time: "01 / 01 / 2021",
+  },
+  {
+    id: 26,
+    category: "kitting",
+    image: "./assets/Image/MachineKnitting/kitting2.jpeg",
+    time: "01 / 01 / 2021",
+  },
+  {
+    id: 27,
+    category: "kitting",
+    image: "./assets/Image/MachineKnitting/kitting3.jpeg",
+    time: "01 / 01 / 2021",
+  },
+  {
+    id: 28,
+    category: "kitting",
+    image: "./assets/Image/MachineKnitting/kitting4.jpeg",
+    time: "01 / 01 / 2021",
+  },
+  {
+    id: 29,
+    category: "kitting",
+    image: "./assets/Image/MachineKnitting/kitting5.jpeg",
+    time: "01 / 01 / 2021",
+  },
+  {
+    id: 30,
+    category: "kitting",
+    image: "./assets/Image/MachineKnitting/kitting6.jpeg",
+    time: "01 / 01 / 2021",
+  },
+  {
+    id: 31,
+    category: "kitting",
+    image: "./assets/Image/MachineKnitting/kitting7.jpeg",
+    time: "01 / 01 / 2021",
+  },
+  {
+    id: 32,
+    category: "kitting",
+    image: "./assets/Image/MachineKnitting/kitting8.jpeg",
+    time: "01 / 01 / 2021",
+  },
+  {
+    id: 33,
+    category: "kitting",
+    image: "./assets/Image/MachineKnitting/kitting9.jpeg",
+    time: "01 / 01 / 2021",
+  },
+  {
+    id: 34,
+    category: "photography",
+    image: "./assets/Image/Photography/photography1.jpeg",
+    time: "01 / 01 / 2021",
+  },
+  {
+    id: 35,
+    category: "photography",
+    image: "./assets/Image/Photography/photography2.jpeg",
+    time: "01 / 01 / 2021",
+  },
+  {
+    id: 36,
+    category: "photography",
+    image: "./assets/Image/Photography/photography3.jpeg",
+    time: "01 / 01 / 2021",
+  },
+  {
+    id: 37,
+    category: "photography",
+    image: "./assets/Image/Photography/photography4.jpeg",
+    time: "01 / 01 / 2021",
+  },
+  {
+    id: 38,
+    category: "photography",
+    image: "./assets/Image/Photography/photography5.jpeg",
+    time: "01 / 01 / 2021",
+  },
+  {
+    id: 39,
+    category: "bodymapping",
+    image: "./assets/Image/Bodymapping/bodymapping1.jpeg",
+    time: "01 / 01 / 2021",
+  },
+  {
+    id: 40,
+    category: "bodymapping",
+    image: "./assets/Image/Bodymapping/bodymapping2.jpeg",
+    time: "01 / 01 / 2021",
+  },
+  {
+    id: 41,
+    category: "bodymapping",
+    image: "./assets/Image/Bodymapping/bodymapping3.jpeg",
+    time: "01 / 01 / 2021",
+  },
+  {
+    id: 42,
+    category: "bodymapping",
+    image: "./assets/Image/Bodymapping/bodymapping4.jpeg",
+    time: "01 / 01 / 2021",
+  },
+  {
+    id: 43,
+    category: "bodymapping",
+    image: "./assets/Image/Bodymapping/bodymapping5.jpeg",
+    time: "01 / 01 / 2021",
+  },
+  {
+    id: 44,
+    category: "bodymapping",
+    image: "./assets/Image/Bodymapping/bodymapping6.jpeg",
+    time: "01 / 01 / 2021",
+  },
+];
+
+// page scroll
+const sections = document.querySelector(".page");
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+  sections.forEach(function (section) {
+    const sectionHeight = section.offsetHeight;
+    const sectionTop = section.offsetTop;
+    sectionId = section.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".navbar a[href*=" + sectionId + "]")
+        .classList.add("active-link");
+    } else {
+      document
+        .querySelector(".navbar a[href*=" + sectionId + "]")
+        .classList.remove("active-link");
+    }
+  });
+}
+window.addEventListener("scroll", scrollActive);
+
+// display images by category
+const sectionCenter = document.querySelector(".section-center");
+const filterBtn = document.querySelectorAll(".filter-btn");
+
+// load items
+window.addEventListener("DOMContentLoaded", function () {
+  displayItems(images);
+});
+
+// filter items
+filterBtn.forEach(function (btn) {
+  btn.addEventListener("click", function (e) {
+    const category = e.currentTarget.dataset.id;
+    const btnCategory = images.filter(function (item) {
+      if (item.category === category) {
+        return item;
+      }
+    });
+    if (category === "all") {
+      displayItems(images);
+    } else {
+      displayItems(btnCategory);
+    }
+  });
+});
+
+function displayItems(items) {
+  let displayPhoto = items.map(function (item) {
+    return `
+      <article class="img-item">
+        <img
+          src=${item.image}
+          class="photo"
+          alt="photo item"
+        />
+        <div class="img-item-info">
+          <h6>Create in: ${item.time}</h6>
+        </div>
+      </article>
+      `;
+  });
+  displayPhoto = displayPhoto.join("");
+  sectionCenter.innerHTML = displayPhoto;
+}
