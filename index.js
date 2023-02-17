989fc96ea5d04782597b92a8c8fee6ecc2fbb9f8
Jeremy Yi <jeremyyiteams@gmail.com>
2023-02-17 13:08:24 +1100

    cv

--- index.js
+++ index.js
@@ -1,317 +1,317 @@
 const images = [
   {
     id: 1,
-    category: "women",
-    image: "./assets/Image/Women/women1.jpeg",
-    time: "01 / 01 / 2021",
+    category: 'women',
+    image: './assets/Image/Women/women1.jpeg',
+    time: '01 / 01 / 2022',
   },
   {
     id: 2,
-    category: "women",
-    image: "./assets/Image/Women/women2.jpeg",
-    time: "01 / 01 / 2021",
+    category: 'women',
+    image: './assets/Image/Women/women2.jpeg',
+    time: '01 / 01 / 2022',
   },
   {
     id: 3,
-    category: "women",
-    image: "./assets/Image/Women/women3.jpeg",
-    time: "01 / 01 / 2021",
+    category: 'women',
+    image: './assets/Image/Women/women3.jpeg',
+    time: '01 / 01 / 2022',
   },
   {
     id: 4,
-    category: "women",
-    image: "./assets/Image/Women/women4.jpeg",
-    time: "01 / 01 / 2021",
+    category: 'women',
+    image: './assets/Image/Women/women4.jpeg',
+    time: '01 / 01 / 2022',
   },
   {
     id: 5,
-    category: "women",
-    image: "./assets/Image/Women/women5.jpeg",
-    time: "01 / 01 / 2021",
+    category: 'women',
+    image: './assets/Image/Women/women5.jpeg',
+    time: '01 / 01 / 2022',
   },
   {
     id: 6,
-    category: "women",
-    image: "./assets/Image/Women/women6.jpeg",
-    time: "01 / 01 / 2021",
+    category: 'women',
+    image: './assets/Image/Women/women6.jpeg',
+    time: '01 / 01 / 2022',
   },
   {
     id: 7,
-    category: "women",
-    image: "./assets/Image/Women/women7.jpeg",
-    time: "01 / 01 / 2021",
+    category: 'women',
+    image: './assets/Image/Women/women7.jpeg',
+    time: '01 / 01 / 2022',
   },
   {
     id: 8,
-    category: "women",
-    image: "./assets/Image/Women/women8.jpeg",
-    time: "01 / 01 / 2021",
+    category: 'women',
+    image: './assets/Image/Women/women8.jpeg',
+    time: '01 / 01 / 2022',
   },
   {
     id: 9,
-    category: "women",
-    image: "./assets/Image/Women/women9.jpeg",
-    time: "01 / 01 / 2021",
+    category: 'women',
+    image: './assets/Image/Women/women9.jpeg',
+    time: '01 / 01 / 2022',
   },
   {
     id: 10,
-    category: "women",
-    image: "./assets/Image/Women/women10.jpeg",
-    time: "01 / 01 / 2021",
+    category: 'women',
+    image: './assets/Image/Women/women10.jpeg',
+    time: '01 / 01 / 2022',
   },
   {
     id: 11,
-    category: "men",
-    image: "./assets/Image/men/men1.jpeg",
-    time: "01 / 01 / 2021",
+    category: 'men',
+    image: './assets/Image/men/men1.jpeg',
+    time: '01 / 01 / 2022',
   },
   {
     id: 12,
-    category: "men",
-    image: "./assets/Image/men/men2.jpeg",
-    time: "01 / 01 / 2021",
+    category: 'men',
+    image: './assets/Image/men/men2.jpeg',
+    time: '01 / 01 / 2022',
   },
   {
     id: 13,
-    category: "men",
-    image: "./assets/Image/men/men3.jpeg",
-    time: "01 / 01 / 2021",
+    category: 'men',
+    image: './assets/Image/men/men3.jpeg',
+    time: '01 / 01 / 2022',
   },
   {
     id: 14,
-    category: "men",
-    image: "./assets/Image/men/men4.jpeg",
-    time: "01 / 01 / 2021",
+    category: 'men',
+    image: './assets/Image/men/men4.jpeg',
+    time: '01 / 01 / 2022',
   },
   {
     id: 15,
-    category: "men",
-    image: "./assets/Image/men/men5.jpeg",
-    time: "01 / 01 / 2021",
+    category: 'men',
+    image: './assets/Image/men/men5.jpeg',
+    time: '01 / 01 / 2022',
   },
   {
     id: 16,
-    category: "men",
-    image: "./assets/Image/men/men6.jpeg",
-    time: "01 / 01 / 2021",
+    category: 'men',
+    image: './assets/Image/men/men6.jpeg',
+    time: '01 / 01 / 2022',
   },
   {
     id: 17,
-    category: "men",
-    image: "./assets/Image/men/men7.jpeg",
-    time: "01 / 01 / 2021",
+    category: 'men',
+    image: './assets/Image/men/men7.jpeg',
+    time: '01 / 01 / 2022',
   },
   {
     id: 18,
-    category: "men",
-    image: "./assets/Image/men/men8.jpeg",
-    time: "01 / 01 / 2021",
+    category: 'men',
+    image: './assets/Image/men/men8.jpeg',
+    time: '01 / 01 / 2022',
   },
   {
     id: 19,
-    category: "bespoke",
-    image: "./assets/Image/Bespoke/bespoke1.jpeg",
-    time: "01 / 01 / 2021",
+    category: 'bespoke',
+    image: './assets/Image/Bespoke/bespoke1.jpeg',
+    time: '01 / 01 / 2022',
   },
   {
     id: 20,
-    category: "bespoke",
-    image: "./assets/Image/Bespoke/bespoke2.jpeg",
-    time: "01 / 01 / 2021",
+    category: 'bespoke',
+    image: './assets/Image/Bespoke/bespoke2.jpeg',
+    time: '01 / 01 / 2022',
   },
   {
     id: 21,
-    category: "bespoke",
-    image: "./assets/Image/Bespoke/bespoke3.jpeg",
-    time: "01 / 01 / 2021",
+    category: 'bespoke',
+    image: './assets/Image/Bespoke/bespoke3.jpeg',
+    time: '01 / 01 / 2022',
   },
   {
     id: 22,
-    category: "bespoke",
-    image: "./assets/Image/Bespoke/bespoke4.jpeg",
-    time: "01 / 01 / 2021",
+    category: 'bespoke',
+    image: './assets/Image/Bespoke/bespoke4.jpeg',
+    time: '01 / 01 / 2022',
   },
   {
     id: 23,
-    category: "bespoke",
-    image: "./assets/Image/Bespoke/bespoke5.jpeg",
-    time: "01 / 01 / 2021",
+    category: 'bespoke',
+    image: './assets/Image/Bespoke/bespoke5.jpeg',
+    time: '01 / 01 / 2022',
   },
   {
     id: 24,
-    category: "bespoke",
-    image: "./assets/Image/Bespoke/bespoke6.jpeg",
-    time: "01 / 01 / 2021",
+    category: 'bespoke',
+    image: './assets/Image/Bespoke/bespoke6.jpeg',
+    time: '01 / 01 / 2022',
   },
   {
     id: 25,
-    category: "kitting",
-    image: "./assets/Image/MachineKnitting/kitting1.jpeg",
-    time: "01 / 01 / 2021",
+    category: 'kitting',
+    image: './assets/Image/MachineKnitting/kitting1.jpeg',
+    time: '01 / 01 / 2022',
   },
   {
     id: 26,
-    category: "kitting",
-    image: "./assets/Image/MachineKnitting/kitting2.jpeg",
-    time: "01 / 01 / 2021",
+    category: 'kitting',
+    image: './assets/Image/MachineKnitting/kitting2.jpeg',
+    time: '01 / 01 / 2022',
   },
   {
     id: 27,
-    category: "kitting",
-    image: "./assets/Image/MachineKnitting/kitting3.jpeg",
-    time: "01 / 01 / 2021",
+    category: 'kitting',
+    image: './assets/Image/MachineKnitting/kitting3.jpeg',
+    time: '01 / 01 / 2022',
   },
   {
     id: 28,
-    category: "kitting",
-    image: "./assets/Image/MachineKnitting/kitting4.jpeg",
-    time: "01 / 01 / 2021",
+    category: 'kitting',
+    image: './assets/Image/MachineKnitting/kitting4.jpeg',
+    time: '01 / 01 / 2022',
   },
   {
     id: 29,
-    category: "kitting",
-    image: "./assets/Image/MachineKnitting/kitting5.jpeg",
-    time: "01 / 01 / 2021",
+    category: 'kitting',
+    image: './assets/Image/MachineKnitting/kitting5.jpeg',
+    time: '01 / 01 / 2022',
   },
   {
     id: 30,
-    category: "kitting",
-    image: "./assets/Image/MachineKnitting/kitting6.jpeg",
-    time: "01 / 01 / 2021",
+    category: 'kitting',
+    image: './assets/Image/MachineKnitting/kitting6.jpeg',
+    time: '01 / 01 / 2022',
   },
   {
     id: 31,
-    category: "kitting",
-    image: "./assets/Image/MachineKnitting/kitting7.jpeg",
-    time: "01 / 01 / 2021",
+    category: 'kitting',
+    image: './assets/Image/MachineKnitting/kitting7.jpeg',
+    time: '01 / 01 / 2022',
   },
   {
     id: 32,
-    category: "kitting",
-    image: "./assets/Image/MachineKnitting/kitting8.jpeg",
-    time: "01 / 01 / 2021",
+    category: 'kitting',
+    image: './assets/Image/MachineKnitting/kitting8.jpeg',
+    time: '01 / 01 / 2022',
   },
   {
     id: 33,
-    category: "kitting",
-    image: "./assets/Image/MachineKnitting/kitting9.jpeg",
-    time: "01 / 01 / 2021",
+    category: 'kitting',
+    image: './assets/Image/MachineKnitting/kitting9.jpeg',
+    time: '01 / 01 / 2022',
   },
   {
     id: 34,
-    category: "photography",
-    image: "./assets/Image/Photography/photography1.jpeg",
-    time: "01 / 01 / 2021",
+    category: 'photography',
+    image: './assets/Image/Photography/photography1.jpeg',
+    time: '01 / 01 / 2022',
   },
   {
     id: 35,
-    category: "photography",
-    image: "./assets/Image/Photography/photography2.jpeg",
-    time: "01 / 01 / 2021",
+    category: 'photography',
+    image: './assets/Image/Photography/photography2.jpeg',
+    time: '01 / 01 / 2022',
   },
   {
     id: 36,
-    category: "photography",
-    image: "./assets/Image/Photography/photography3.jpeg",
-    time: "01 / 01 / 2021",
+    category: 'photography',
+    image: './assets/Image/Photography/photography3.jpeg',
+    time: '01 / 01 / 2022',
   },
   {
     id: 37,
-    category: "photography",
-    image: "./assets/Image/Photography/photography4.jpeg",
-    time: "01 / 01 / 2021",
+    category: 'photography',
+    image: './assets/Image/Photography/photography4.jpeg',
+    time: '01 / 01 / 2022',
   },
   {
     id: 38,
-    category: "photography",
-    image: "./assets/Image/Photography/photography5.jpeg",
-    time: "01 / 01 / 2021",
+    category: 'photography',
+    image: './assets/Image/Photography/photography5.jpeg',
+    time: '01 / 01 / 2022',
   },
   {
     id: 39,
-    category: "bodymapping",
-    image: "./assets/Image/Bodymapping/bodymapping1.jpeg",
-    time: "01 / 01 / 2021",
+    category: 'bodymapping',
+    image: './assets/Image/Bodymapping/bodymapping1.jpeg',
+    time: '01 / 01 / 2022',
   },
   {
     id: 40,
-    category: "bodymapping",
-    image: "./assets/Image/Bodymapping/bodymapping2.jpeg",
-    time: "01 / 01 / 2021",
+    category: 'bodymapping',
+    image: './assets/Image/Bodymapping/bodymapping2.jpeg',
+    time: '01 / 01 / 2022',
   },
   {
     id: 41,
-    category: "bodymapping",
-    image: "./assets/Image/Bodymapping/bodymapping3.jpeg",
-    time: "01 / 01 / 2021",
+    category: 'bodymapping',
+    image: './assets/Image/Bodymapping/bodymapping3.jpeg',
+    time: '01 / 01 / 2022',
   },
   {
     id: 42,
-    category: "bodymapping",
-    image: "./assets/Image/Bodymapping/bodymapping4.jpeg",
-    time: "01 / 01 / 2021",
+    category: 'bodymapping',
+    image: './assets/Image/Bodymapping/bodymapping4.jpeg',
+    time: '01 / 01 / 2022',
   },
   {
     id: 43,
-    category: "bodymapping",
-    image: "./assets/Image/Bodymapping/bodymapping5.jpeg",
-    time: "01 / 01 / 2021",
+    category: 'bodymapping',
+    image: './assets/Image/Bodymapping/bodymapping5.jpeg',
+    time: '01 / 01 / 2022',
   },
   {
     id: 44,
-    category: "bodymapping",
-    image: "./assets/Image/Bodymapping/bodymapping6.jpeg",
-    time: "01 / 01 / 2021",
+    category: 'bodymapping',
+    image: './assets/Image/Bodymapping/bodymapping6.jpeg',
+    time: '01 / 01 / 2022',
   },
-];
+]
 
 // page scroll
-const sections = document.querySelector(".page");
+const sections = document.querySelector('.page')
 function scrollActive() {
-  const scrollY = window.pageYOffset;
+  const scrollY = window.pageYOffset
   sections.forEach(function (section) {
-    const sectionHeight = section.offsetHeight;
-    const sectionTop = section.offsetTop;
-    sectionId = section.getAttribute("id");
+    const sectionHeight = section.offsetHeight
+    const sectionTop = section.offsetTop
+    sectionId = section.getAttribute('id')
 
     if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
       document
-        .querySelector(".navbar a[href*=" + sectionId + "]")
-        .classList.add("active-link");
+        .querySelector('.navbar a[href*=' + sectionId + ']')
+        .classList.add('active-link')
     } else {
       document
-        .querySelector(".navbar a[href*=" + sectionId + "]")
-        .classList.remove("active-link");
+        .querySelector('.navbar a[href*=' + sectionId + ']')
+        .classList.remove('active-link')
     }
-  });
+  })
 }
-window.addEventListener("scroll", scrollActive);
+window.addEventListener('scroll', scrollActive)
 
 // display images by category
-const sectionCenter = document.querySelector(".section-center");
-const filterBtn = document.querySelectorAll(".filter-btn");
+const sectionCenter = document.querySelector('.section-center')
+const filterBtn = document.querySelectorAll('.filter-btn')
 
 // load items
-window.addEventListener("DOMContentLoaded", function () {
-  displayItems(images);
-});
+window.addEventListener('DOMContentLoaded', function () {
+  displayItems(images)
+})
 
 // filter items
 filterBtn.forEach(function (btn) {
-  btn.addEventListener("click", function (e) {
-    const category = e.currentTarget.dataset.id;
+  btn.addEventListener('click', function (e) {
+    const category = e.currentTarget.dataset.id
     const btnCategory = images.filter(function (item) {
       if (item.category === category) {
-        return item;
+        return item
       }
-    });
-    if (category === "all") {
-      displayItems(images);
+    })
+    if (category === 'all') {
+      displayItems(images)
     } else {
-      displayItems(btnCategory);
+      displayItems(btnCategory)
     }
-  });
-});
+  })
+})
 
 function displayItems(items) {
   let displayPhoto = items.map(function (item) {
@@ -326,8 +326,12 @@ function displayItems(items) {
           <h6>Create in: ${item.time}</h6>
         </div>
       </article>
-      `;
-  });
-  displayPhoto = displayPhoto.join("");
-  sectionCenter.innerHTML = displayPhoto;
+      `
+  })
+  displayPhoto = displayPhoto.join('')
+  sectionCenter.innerHTML = displayPhoto
 }
+
+let date = new Date()
+let year = date.getFullYear()
+document.getElementById('currentYear').innerHTML = year
